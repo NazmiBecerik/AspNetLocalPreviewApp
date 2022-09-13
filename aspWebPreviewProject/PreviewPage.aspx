548e216d09675087595120545736e3b98f3f2440
Nazmi Becerik <nazmibecerik@outlook.com>
2022-09-13 14:14:21 +0300

    Add project files.

---
+++ aspWebPreviewProject/PreviewPage.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PreviewPage.aspx.cs" Inherits="aspWebPreviewProject.PreviewPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <title>Preview Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="row">
+            <asp:Button CssClass="col-sm-12" ID="Button1" runat="server" Text="Görüntüleri Gör" OnClick="Button1_Click" Style="margin-left: 15px" />
+
+        </div>
+        <div class="display:flex">
+            <div class="row" style="margin-left: 0px; margin-right: 0px">
+
+                <div class="col-sm-6">
+                    <ul class="list-group">
+                        <li class="list-group-item  list-group-item-primary">Kumaş Türü</li>
+                        <%--<li id="fabricTypeDiv" class="list-group-item" runat="server"/>--%>
+                        <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="doubleClick"></asp:ListBox>
+                    </ul>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="list-group">
+                        <li class="list-group-item list-group-item-primary">Tarihi</li>
+                        <%--<li class="list-group-item list-group-item-primary">This is a primary list group item</li>--%>
+                        <asp:ListBox ID="ListBox2" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ListBox2Click"></asp:ListBox>
+                    </ul>
+                </div>
+            </div>
+            <div class="row" style="margin-left: 0px; margin-right: 0px">
+                <asp:Table runat="server" ID="table1">
+                  
+                </asp:Table>
+            </div>
+        </div>
+
+
+
+        <%--<div class="col-sm-4">--%>
+        <%--<ul class="list-group">--%>
+        <%--<li class="list-group-item list-group-item-primary">Bulunan Hatalar</li>--%>
+        <%--<li class="list-group-item list-group-item-primary">This is a primary list group item</li>--%>
+        <asp:ListBox ID="ListBox3" runat="server" Visible="false" AutoPostBack="True" OnSelectedIndexChanged="ListBox3Click"></asp:ListBox>
+        <%--</ul>--%>
+        <%--</div>--%>
+        <%--</div>--%>
+    </form>
+    <script>
+        //function OpenWindow(kumasfolder,datefolder,fileName)
+        //{
+        //    window.open('http://localhost/GORUNTULER/' + kumasfolder +'/' + datefolder +'/'+fileName+'', 'top', 'height=700,width=800,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes');
+
+        //}
+        function OpenWindow(base64) {
+            let pdfWindow = window.open("")
+            pdfWindow.document.write(
+                "<iframe allowFullScreen='true' title='Hata Görüntüleme Sayfası' width='1920' height='1100' src='data:image/jpeg;base64, " +
+                encodeURI(base64) + "'></iframe>"
+            )
+
+
+        }
+    </script>
+</body>
+</html>
---
+++ aspWebPreviewProject/PreviewPage.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspWebPreviewProject
+{
+    public partial class PreviewPage : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        public static string globalPath = @"C:\inetpub\wwwroot\";
+        public static string pathLbox1 = "";
+        public static string pathLbox2 = "";
+       
+
+        protected void doubleClick(object sender, EventArgs e)
+        {
+            if (ListBox1.SelectedItem != null)
+            {
+                ListBox3.Items.Clear();
+                ListBox2.Items.Clear();
+                pathLbox1= ListBox1.SelectedItem.Text.ToString();
+                GetSubDirectories();
+
+
+            }
+        }
+
+        protected void ListBox2Click(object sender, EventArgs e)
+        {
+          
+            if (ListBox2.SelectedItem != null)
+            {
+
+                pathLbox2= ListBox2.SelectedItem.Text.ToString();
+                GetSubList2Directories();
+                globalPath += pathLbox1 + '/' + pathLbox2+'/';
+                FilledTable(globalPath);
+            }
+        }
+
+        //public void OpenImage()
+        //{
+        //    string FilePath = "http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Value + "/" + ListBox2.SelectedItem.Text + "/" + ListBox3.SelectedItem.Text;
+
+        //    Response.Write("<script>");
+        //    Response.Write("window.open('" + FilePath + "','top', 'height=700,width=800,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes');");
+        //    Response.Write("</script>");
+        //}
+
+        protected void ListBox3Click(object sender, EventArgs e)
+        {
+            if (ListBox3.SelectedItem != null)
+            {
+
+                string FilePath = "http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Value + "/" + ListBox2.SelectedItem.Text + "/" + ListBox3.SelectedItem.Text;
+                Response.Write("<script>");
+                Response.Write("window.open('" + FilePath + "','top', 'height=700,width=800,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes');");
+                Response.Write("</script>");
+            
+            }
+        }
+
+        public void FilledTable(string path)
+        {
+            DirectoryInfo directoriesForFilledTable = new DirectoryInfo(@"C:\inetpub\wwwroot\GORUNTULER\" + ListBox1.SelectedItem.Text + "\\" + ListBox2.SelectedItem.Text);
+            var files = directoriesForFilledTable.GetFiles();
+            TableRow tRow = new TableRow();
+            tRow.Attributes.CssStyle["margin-bottom"] = "10px";
+            table1.Rows.Add(tRow); 
+            int row = 0;
+            int cells = 0;
+            for (int i = 0; i < files.Length; i++)
+            {
+                
+                if (i % 5 == 0)
+                {
+                    TableRow tRow2 = new TableRow();
+                    tRow2.Attributes.CssStyle["margin-bottom"] = "10px";
+                    table1.Rows.Add(tRow2);
+                    if (i != 0)
+                    {
+                        row = row + 1;
+                        cells = 0;
+                    }
+                }
+                if ((files[i].Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                {
+                  // var filePath = FileExtensionConverter.ExtensionConverter(files[i].FullName);
+                    TableCell tCell = new TableCell();                   
+                    table1.Rows[row].Cells.Add(tCell);
+                    
+                    ImageButton imgBtn = new ImageButton();
+                    Label lbl = new Label();
+                    lbl.Text = files[i].Name;
+                    //imgBtn.ImageUrl = "http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Text.ToString() + '/' + ListBox2.SelectedItem.Text.ToString() + '/' + i;
+                    //imgBtn.ImageUrl = "http://localhost/GORUNTULER/512002-002/01.01.2022/images.png";
+
+                    System.Net.WebRequest request = System.Net.WebRequest.Create(@"http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Text + "//" + ListBox2.SelectedItem.Text + "/" + files[i].Name);
+                    //System.Net.WebRequest request = System.Net.WebRequest.Create(filePath);
+                    System.Net.WebResponse response = request.GetResponse();
+                    System.IO.Stream responseStream = response.GetResponseStream();
+                    Bitmap bitmap2 = new Bitmap(responseStream);
+
+                    var imageHeight = 1200;
+                    var imageWidth = 1920;
+                    
+                    System.IO.MemoryStream ms = new MemoryStream();
+                    bitmap2.Save(ms, ImageFormat.Jpeg);
+                    byte[] byteImage = ms.ToArray();
+                    var SigBase64 = Convert.ToBase64String(byteImage);
+                    imgBtn.ImageUrl = "data:image/png;base64," + SigBase64;
+                    //imgBtn.ImageUrl = @"http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Text + "//" + ListBox2.SelectedItem.Text + "/"+files[i].Name;
+                   // imgBtn.ImageUrl = @"http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Text + "//" + ListBox2.SelectedItem.Text + "/"+fileNameWithExtension;
+                    
+                    imgBtn.Height = imageHeight/5;
+                    imgBtn.Width = imageWidth/5;
+                    imgBtn.ID = "imgBtn" + i;
+                    imgBtn.OnClientClick = "OpenWindow('" + SigBase64 + "');return false;";
+                    //imgBtn.OnClientClick = "OpenWindow('" + ListBox1.SelectedItem.Text + "','" + ListBox2.SelectedItem.Text + "','" + files[i].Name+"');return false;///";
+ //çalışankodblogu  imgBtn.OnClientClick = "OpenWindow('" + ListBox1.SelectedItem.Text + "','" + ListBox2.SelectedItem.Text + "','" + files[i].Name+"');return false;";
+                    //imgBtn.OnClientClick = "OpenWindow('" + ListBox1.SelectedItem.Text + "','" + ListBox2.SelectedItem.Text + "','" + files[i].Name + "');return false;";
+
+                    //imgBtn.CommandArgument = @"http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Text + "//" + ListBox2.SelectedItem.Text + "/" + files[i].Name;
+                    //imgBtn.Command += new CommandEventHandler(OnImageClick);
+                    //imgBtn.Attributes.Add("onclick", "return false;");
+                    //imgBtn.Attributes.Add("AutoPostBack", "false");
+                    //table1.Rows[0].Cells[i].Text = string.Format("<img src='' width='200' height ='200' id ='photoReview' runat='server'></img>");
+                    table1.Rows[row].Cells[cells].Controls.Add(imgBtn);
+                    table1.Rows[row].Cells[cells].Controls.Add(lbl);
+                    table1.Rows[row].Cells[cells].Attributes.CssStyle["text-align"] = "center";
+                    //   fabricTypeDiv.InnerHtml += "<li class='list-group-item' onClick=\"alert('selam')\" >" + dir.ToString() + "</li>";
+                }
+                // ListBox1.Items.Add(dirArray[i].ToString());
+                cells = cells + 1;
+
+            }
+
+        }
+        string fileName = "";
+        protected void OnImageClick(object sender, CommandEventArgs e)
+        {
+            fileName = e.CommandName;
+            
+        }
+        public void GetDirectories()
+        {
+            
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\inetpub\wwwroot\GORUNTULER");
+            DirectoryInfo[] dirArray = dirInfo.GetDirectories();
+            foreach (var directory in dirArray)
+            {
+                ListBox1.Items.Add(directory.Name);
+            }                  
+        }
+        public void GetSubDirectories()
+        {
+            ListBox2.Items.Clear();
+            DirectoryInfo subDirInfo = new DirectoryInfo(@"C:\inetpub\wwwroot\GORUNTULER\" + pathLbox1);
+            DirectoryInfo[] subDirArray = subDirInfo.GetDirectories();
+
+            foreach (var subdir in subDirArray)
+            {
+                ListBox2.Items.Add(subdir.ToString());
+            }
+        }
+
+        
+
+        
+
+        public void GetSubList2Directories()
+        {
+            ListBox3.Items.Clear();
+            DirectoryInfo subDirInfo = new DirectoryInfo(@"C:\inetpub\wwwroot\GORUNTULER\" + ListBox1.SelectedItem.Text + "\\" + ListBox2.SelectedItem.Text);
+            //globalPath2 = @"http://localhost/GORUNTULER/" + ListBox1.SelectedItem.Text + "/" + globalPath;
+            var subDirArray = subDirInfo.GetFiles();
+
+            for (int i = 0; i < subDirArray.Length; i++)
+            {
+                ListBox3.Items.Add(subDirArray[i].Name);
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            GetDirectories();
+        }
+    }
+}
